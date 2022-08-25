c7e1a6b4df3ec5c1a1b642c60115068cbd83f56b
f-i-t-s-u-m <fibabirr@gmail.com>
2022-08-25 13:03:48 +0300

    new db.js

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,11 @@ import { MongoClient } from 'mongodb';
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.SB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'file_manager';
+const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
-    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect().then(() => {
       this.db = this.client.db(database);
     }).catch((error) => console.log(error));
@@ -17,14 +18,15 @@ class DBClient {
   }
 
   async nbUsers() {
-    const collection = this.db.collection('users');
-    const countUsers = await collection.countDocuments();
+    const users = this.db.collection('users');
+    console.log(users);
+    const countUsers = await users.countDocuments();
     return countUsers;
   }
 
   async nbFiles() {
-    const collection = this.db.collection('files');
-    const countFiles = await collection.countDocuments();
+    const files = this.db.collection('files');
+    const countFiles = await files.countDocuments();
     return countFiles;
   }
 }
